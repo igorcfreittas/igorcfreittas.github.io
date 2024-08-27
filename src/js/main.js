6c365b1409b268fde96a821d773e255cbc464409
Igor Correia Freitas <igorcfreittas@gmail.com>
2024-08-27 17:46:57 -0300

    Inserting the header

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    const SKILL_BAR_LENGTH_IN_YEARS = 10;
+    const LANGUAGES_PROPERTIES = {
+        'backend': [
+            {
+                'language': 'PHP',
+                'experience_in_years': 8
+            },
+            {
+                'language': 'python',
+                'experience_in_years': 2
+            }
+        ]
+    };
+
+};
